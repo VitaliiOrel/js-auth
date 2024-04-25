e556d5b17dbc078078dfa39e61b6e2d61b67d4a1
VitaliiOrel <orelmsg@gmail.com>
2024-04-25 18:09:14 +0200

    js-auth-part2

--- src/container/signup/index.js
+++ src/container/signup/index.js
@@ -1,20 +1,106 @@
 console.log('SignupForm')
 
-class SignupForm {
-  static value = { 1: 1 }
+import {
+  Form,
+  REG_EXP_EMAIL,
+  REG_EXP_PASSWORD,
+} from '../../script/form'
 
-  static validate = (name, value) => {
-    return true
+class SignupForm extends Form {
+  FIELD_NAME = {
+    EMAIL: 'email',
+    PASSWORD: 'password',
+    PASSWORD_AGAIN: 'passwordAgain',
+    ROLE: 'role',
+    IS_CONFIRM: 'isConfirm',
   }
 
-  static submit = () => {
-    console.log(this.value)
+  FIELD_ERROR = {
+    IS_EMPTY: 'Enter a value',
+    IS_BIG: 'The value is too long',
+    EMAIL: 'Enter correct value',
+    PASSWORD: 'Password must consist at least 8 characters',
+    PASSWORD_AGAIN: 'It does not correspond to the first.',
+    NOT_CONFIRM: 'You need to confirm the rules',
+    ROLE: 'You need to choose a role',
   }
 
-  static change = (name, value) => {
-    console.log(name, value)
-    if (this.validate(name, value)) this.value[name] = value
+  validate = (name, value) => {
+    if (String(value).length < 1) {
+      return this.FIELD_ERROR.IS_EMPTY
+    }
+    if (String(value).length > 20) {
+      return this.FIELD_ERROR.IS_BIG
+    }
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(value)) {
+        return this.FIELD_ERROR.EMAIL
+      }
+    }
+    if (name === this.FIELD_NAME.PASSWORD) {
+      if (!REG_EXP_PASSWORD.test(value)) {
+        return this.FIELD_ERROR.PASSWORD
+      }
+    }
+    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
+      if (
+        String(value) !==
+        this.value[this.FIELD_NAME.PASSWORD]
+      ) {
+        return this.FIELD_ERROR.PASSWORD_AGAIN
+      }
+    }
+    if (name === this.FIELD_NAME.IS_CONFIRM) {
+      if (Boolean(value) !== true) {
+        return this.FIELD_ERROR.NOT_CONFIRM
+      }
+    }
+    if (name === this.FIELD_NAME.ROLE) {
+      if (isNaN(value)) {
+        return this.FIELD_ERROR.ROLE
+      }
+    }
+  }
+
+  submit = async () => {
+    if (this.disabled) {
+      this.validateAll()
+    } else {
+      console.log(this.value)
+      this.setAlert('progress', 'Loading data...')
+
+      try {
+        const res = await fetch('/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: this.convertData(),
+        })
+
+        const data = await res.json()
+
+        if (res.ok) {
+          this.setAlert('success', data.message)
+        } else {
+          this.setAlert('error', data.message)
+        }
+      } catch (error) {
+        this.setAlert('error', error.message)
+      }
+    }
+  }
+
+  convertData = () => {
+    return JSON.stringify({
+      [this.FIELD_NAME.EMAIL]:
+        this.value[this.FIELD_NAME.EMAIL],
+      [this.FIELD_NAME.PASSWORD]:
+        this.value[this.FIELD_NAME.PASSWORD],
+      [this.FIELD_NAME.ROLE]:
+        this.value[this.FIELD_NAME.ROLE],
+    })
   }
 }
 
-window.signupForm = SignupForm
+window.signupForm = new SignupForm()
--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,11 @@ const router = express.Router()
 
 const User = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  role: 1,
+})
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -43,6 +48,30 @@ router.get('/signup', function (req, res) {
     },
   })
   // ↑↑ сюди вводимо JSON дані
+})
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: 'Error, required data missing',
+    })
+  }
+
+  try {
+    User.create({ email, password, role })
+
+    return res
+      .status(200)
+      .json({ message: 'User registered' })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Error, required data missing',
+    })
+  }
 })
 
 // Підключаємо роутер до бек-енду
